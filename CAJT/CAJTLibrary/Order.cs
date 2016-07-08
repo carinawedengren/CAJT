62b6cdba3f8fe314db5f7a8f411b8bd140e5ae90
anddan1106 <anddan1106@gmail.com>
2016-07-08 15:35:17 +0200

    lade till klasser och proppar till order-klassen

---
+++ CAJT/CAJTLibrary/Order.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAJTLibrary
+{
+    public class Order
+    {
+        public int OrderNumber
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public List<Product> Products
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public Customer Customer
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public Status Status
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public bool Paid
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            private set
+            {
+            }
+        }
+
+        public double TotalAmount()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    public enum Status
+    {
+        Pending,
+        Processing,
+        Deliverd
+    }
+}
